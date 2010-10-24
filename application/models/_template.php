dfe2bae08d5990f50349a5c32f18ff7c8b1741c4
WanWizard <devnull@localhost>
2010-10-24 11:12:10 +0200

    added the post_model_init() method to the _template model updated the documentation to include the usage of the post_model_init() method updated the documentation to include the usage of a fully qualified producation cache path

--- application/models/_template.php
+++ application/models/_template.php
@@ -6,7 +6,7 @@
  * Use this basic model as a template for creating new models.
  * It is not recommended that you include this file with your application,
  * especially if you use a Template library (as the classes may collide).
- * 
+ *
  * To use:
  * 1) Copy this file to the lowercase name of your new model.
  * 2) Find-and-replace (case-sensitive) 'Template' with 'Your_model'
@@ -20,30 +20,30 @@
  * @link		http://www.overzealous.com
  */
 class Template extends DataMapper {
-	
+
 	// Uncomment and edit these two if the class has a model name that
 	//   doesn't convert properly using the inflector_helper.
 	// var $model = 'template';
 	// var $table = 'templates';
-	
+
 	// You can override the database connections with this option
 	// var $db_params = 'db_config_name';
-	
+
 	// --------------------------------------------------------------------
 	// Relationships
 	//   Configure your relationships below
 	// --------------------------------------------------------------------
-	
+
 	// Insert related models that Template can have just one of.
 	var $has_one = array();
-	
+
 	// Insert related models that Template can have more than one of.
 	var $has_many = array();
-	
+
 	/* Relationship Examples
 	 * For normal relationships, simply add the model name to the array:
-	 *   $has_one = array('user'); // Template has one User 
-	 * 
+	 *   $has_one = array('user'); // Template has one User
+	 *
 	 * For complex relationships, such as having a Creator and Editor for
 	 * Template, use this form:
 	 *   $has_one = array(
@@ -52,17 +52,17 @@ class Template extends DataMapper {
 	 *   		'other_field' => 'created_template'
 	 *   	)
 	 *   );
-	 *   
+	 *
 	 * Don't forget to add 'created_template' to User, with class set to
 	 * 'template', and the other_field set to 'creator'!
-	 * 
+	 *
 	 */
-	
+
 	// --------------------------------------------------------------------
 	// Validation
 	//   Add validation requirements, such as 'required', for your fields.
 	// --------------------------------------------------------------------
-	
+
 	var $validation = array(
 		'example' => array(
 			// example is required, and cannot be more than 120 characters long.
@@ -70,15 +70,15 @@ class Template extends DataMapper {
 			'label' => 'Example'
 		)
 	);
-	
+
 	// --------------------------------------------------------------------
 	// Default Ordering
 	//   Uncomment this to always sort by 'name', then by
 	//   id descending (unless overridden)
 	// --------------------------------------------------------------------
-	
+
 	// var $default_order_by = array('name', 'id' => 'desc');
-	
+
 	// --------------------------------------------------------------------
 
 	/**
@@ -88,24 +88,33 @@ class Template extends DataMapper {
 	{
 		parent::__construct($id);
     }
-	
+
+	// --------------------------------------------------------------------
+	// Post Model Initialisation
+	//   Add your own custom initialisation code to the Model
+	// The parameter indicates if the current config was loaded from cache or not
+	// --------------------------------------------------------------------
+	function post_model_init($from_cache = FALSE)
+	{
+	}
+
 	// --------------------------------------------------------------------
 	// Custom Methods
 	//   Add your own custom methods here to enhance the model.
 	// --------------------------------------------------------------------
-	
+
 	/* Example Custom Method
 	function get_open_templates()
 	{
 		return $this->where('status <>', 'closed')->get();
 	}
 	*/
-	
+
 	// --------------------------------------------------------------------
 	// Custom Validation Rules
 	//   Add custom validation rules for this model here.
 	// --------------------------------------------------------------------
-	
+
 	/* Example Rule
 	function _convert_written_numbers($field, $parameter)
 	{
