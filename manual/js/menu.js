9854864905139b151b3237b4320a647e8bf01b12
WanWizard <devnull@localhost>
2012-05-10 22:47:39 +0200

    fixed loading the donate button

--- manual/js/menu.js
+++ manual/js/menu.js
@@ -224,8 +224,9 @@ var Menu = new Class({
 
 		// add donate box
 		var td = document.createElement('td');
+		url = document.location.pathname.replace(/(^.*\/)(.*)$/, '$1../images/donate_button.gif');
 		td.id = "paypal";
-		td.innerHTML = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_donations"><input type="hidden" name="business" value="paypal@wanwizard.eu"><input type="hidden" name="lc" value="GB"><input type="hidden" name="no_note" value="0"><input type="hidden" name="item_name" value="One-time donation to support the development of DataMapper"><input type="hidden" name="currency_code" value="EUR"><input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest"><input type="image" src="./images/donate_button.gif" height="30" "border="0" name="submit" alt="PayPal — The safer, easier way to pay online."><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>';
+		td.innerHTML = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_donations"><input type="hidden" name="business" value="paypal@wanwizard.eu"><input type="hidden" name="lc" value="GB"><input type="hidden" name="no_note" value="0"><input type="hidden" name="item_name" value="One-time donation to support the development of DataMapper"><input type="hidden" name="currency_code" value="EUR"><input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest"><input type="image" src="' + url + '" height="30" "border="0" name="submit" alt="PayPal — The safer, easier way to pay online."><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>';
 		bc.parentNode.appendChild(td);
 	}
 });
