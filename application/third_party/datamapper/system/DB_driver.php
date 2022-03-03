84058b1d7ee2ad7f32d8c8b0f1accf948d78bb75
Jamie Burchell <jamie@ib3.uk>
2022-03-03 22:49:27 +0000

    Use splat operator to support any number of params

--- application/third_party/datamapper/system/DB_driver.php
+++ application/third_party/datamapper/system/DB_driver.php
@@ -23,28 +23,9 @@ if ( ! class_exists($driver, FALSE) )
 class $driver extends $org_driver
 {
 	// public interface to internal driver methods
-	public function dm_call_method(\$function, \$p1 = null, \$p2 = null, \$p3 = null, \$p4 = null, \$p5 = null)
+	public function dm_call_method(\$function, ...\$args)
 	{
-		switch (func_num_args())
-		{
-			case 1:
-				return \$this->{\$function}();
-			case 2:
-				return \$this->{\$function}(\$p1);
-				break;
-			case 3:
-				return \$this->{\$function}(\$p1, \$p2);
-				break;
-			case 4:
-				return \$this->{\$function}(\$p1, \$p2, \$p3);
-				break;
-			case 5:
-				return \$this->{\$function}(\$p1, \$p2, \$p3, \$p4);
-				break;
-			case 6:
-				return \$this->{\$function}(\$p1, \$p2, \$p3, \$p4, \$p5);
-				break;
-		}
+		return \$this->{\$function}(...\$args);
 	}
 
 	// public interface to internal driver properties
