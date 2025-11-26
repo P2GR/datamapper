f84c87cb1d5398a295d1c217c5e04fcc5d76a103
Lennart <lp@logboekenonline.nl>
2025-11-26 10:09:47 +0100

    fix: removal of lang files

--- application/language/nl/number_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang['terabyte_abbr']	= 'TB';
-$lang['gigabyte_abbr']	= 'GB';
-$lang['megabyte_abbr']	= 'MB';
-$lang['kilobyte_abbr']	= 'kB';
-$lang['bytes']		= 'Bytes';
--- application/language/nl/pagination_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang['pagination_first_link']	= '&lsaquo; Eerste';
-$lang['pagination_next_link']	= '&gt;';
-$lang['pagination_prev_link']	= '&lt;';
-$lang['pagination_last_link']	= 'Laatste &rsaquo;';
